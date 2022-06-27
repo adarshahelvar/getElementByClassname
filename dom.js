3bbdca55ef7b59fa36af720370e851c2e463a870
Adarsha A <108178521+adarshahelvar@users.noreply.github.com>
2022-06-27 19:47:09 +0530

    Create dom.js

---
+++ dom.js
@@ -0,0 +1,5 @@
+var itmes= document.getElementsByClassName('list-group-item');
+itmes[2].style.backgroundColor='green';
+for(var i=0; i<itmes.length; i++) {
+    itmes[i].style.backgroundColor='#f4f4f4';
+}
